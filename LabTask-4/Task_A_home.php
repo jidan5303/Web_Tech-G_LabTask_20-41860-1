7dd28193d217bebcfcb369cc14a0f3e50864d1d8
Md. Jidan Rahman <66253682+jidan5303@users.noreply.github.com>
2022-06-24 13:20:22 +0600

    Lab Task-4 uploaded

---
+++ LabTask-4/Task_A_home.php
@@ -0,0 +1,21 @@
+<?php
+include 'header footer.php';
+?>
+
+<!DOCTYPE html>  
+<html>  
+    <head>  
+        <title>Registration</title>  
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+    </head>
+    <body>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div class="container" style="width:500px;">
+        
+            <h1>Welcome to VojonBilash</h1>
+        
+        </div>
+    </body>
+</html>
